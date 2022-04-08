4e99f09827dd3b242890aec73131b4c806dbad32
JorgeBeltr <201502274@est.umss.edu>
2022-04-08 09:34:59 -0400

    mejora de validaciones
    se mejoro las validaciones y se impelento las imagenes de pedido comodo

--- app/models/vendedor.rb
+++ app/models/vendedor.rb
@@ -2,16 +2,18 @@ class Vendedor < ApplicationRecord
     has_one_attached:imagen
     mount_uploader :perfil, PerfilUploader
     validates :Nombre, :Apellido_Paterno, :Apellido_Materno, :Telefono, :Mail, :Direccion, presence: {message:"debe estar llenado"}
-    validates :Nombre, :Apellido_Paterno, :Apellido_Materno, format: { with: /\A[a-zA-Z]+\z/, message: "solo acepta letras" }
-    validates :Nombre, length: { in: 2..50 , message: "tiene que tener minimo 2 letras"}
-    validates :Apellido_Materno, length: { in: 2..20 , message: "Tiene que tener minimo 2 letras y maximo 20"}
-    validates :Apellido_Paterno, length: { in: 2..20 , message: "Tiene que tener minimo 2 letras y maximo 20"}
+    validates :Nombre, :Apellido_Paterno, :Apellido_Materno, format: { with: /\A[a-zA-Z]+(?:[\s-]*\w+)?\z/, message: "solo acepta letras y maximo 2 palabras" }
+    validates :Nombre, length: { in: 2..21 , message: "tiene que tener minimo 2 letras y maximo 21"}
+    validates :Apellido_Materno, length: { in: 2..21 , message: "Tiene que tener minimo 2 letras y maximo 21"}
+    validates :Apellido_Paterno, length: { in: 2..21 , message: "Tiene que tener minimo 2 letras y maximo 21"}
     validates :Telefono, numericality: { only_integer: true , message:"solo acepta Números"}
     validates :Telefono, length: {is: 8 , message: "Tiene que tener 8 numeros"}
     validates :Telefono, numericality: {in: 60000000..79999999 , message: "Tiene que empezar con 6 o 7"}
     validates :Mail, email_format: { message: 'tiene que ser como el siguiente ejemplo, juan@example.com' }
-    validates :Direccion, url: { allow_nil: true, message: "tiene que ingresar una url" }
+    validates :Direccion, length: { in: 10..40 , message: "tiene que tener minimo 10 letras y maximo 40"}
+    #validates :Direccion, url: { allow_nil: true, message: "tiene que ingresar una url" }
     validates :Telefono, :Mail, uniqueness: {message:"ya existe usuario"}
+    #validates :Direccion , format: {with: /maps/ , message: "el link tiene que tener el siguiete formato: https://goo.gl/maps" }
     validates :imagen, content_type: [:png, :jpg, :jpeg]
 
 end
