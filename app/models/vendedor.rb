de627c6062c18a0ab159bcd7a3e265cd78b482ed
GonzaZeballos <101687891+GonzaZeballos@users.noreply.github.com>
2022-04-15 21:21:14 -0400

    Correcciones ortográficas

--- app/models/vendedor.rb
+++ app/models/vendedor.rb
@@ -5,11 +5,11 @@ class Vendedor < ApplicationRecord
     validates :Nombre, format: { with: /\A[a-zA-Z-ÿ\u00f1\u00d1\u00E0-\u00FC]{2,}(?:[\s-]*[a-zA-Z-ÿ\u00f1\u00d1\u00E0-\u00FC]+)?{2,}(?:[\s-]*[a-zA-Z-ÿ\u00f1\u00d1\u00E0-\u00FC]{2,})?\z/, message: "solo acepta letras y máximo 3 palabras, cada palabra debe tener mínimo 2 letras" }
     validates :Apellido_Paterno, :Apellido_Materno, format: { with: /\A[a-zA-Z-ÿ\u00f1\u00d1\u00E0-\u00FC]{2,}(?:[\s-]*[a-zA-Z-ÿ\u00f1\u00d1\u00E0-\u00FC]{2,})?\z/, message: "solo acepta letras y máximo 2 palabras, cada palabra debe tener mínimo 2 letras" }
     validates :Nombre, length: { in: 2..21 , message: "tiene que tener mínimo 2 letras y máximo 21"}
-    validates :Apellido_Materno, length: { in: 2..21 , message: "Tiene que tener mínimo 2 letras y máximo 21"}
-    validates :Apellido_Paterno, length: { in: 2..21 , message: "Tiene que tener mínimo 2 letras y máximo 21"}
-    validates :Telefono, numericality: { only_integer: true , message:"solo acepta Números"}
-    validates :Telefono, length: {is: 8 , message: "Tiene que tener 8 números"}
-    validates :Telefono, numericality: {in: 60000000..79999999 , message: "Tiene que empezar con 6 o 7"}
+    validates :Apellido_Materno, length: { in: 2..21 , message: "tiene que tener mínimo 2 letras y máximo 21"}
+    validates :Apellido_Paterno, length: { in: 2..21 , message: "tiene que tener mínimo 2 letras y máximo 21"}
+    validates :Telefono, numericality: { only_integer: true , message:"solo acepta números"}
+    validates :Telefono, length: {is: 8 , message: "tiene que tener 8 números"}
+    validates :Telefono, numericality: {in: 60000000..79999999 , message: "tiene que empezar con 6 o 7"}
     validates :Mail, email_format: {with: /\A\w+(\.*)\w*+([@\s]*)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, message: 'tiene que ser como el siguiente ejemplo: juan@example.com' }
     validates :Direccion, length: { in: 10..40 , message: "tiene que tener mínimo 10 letras y máximo 40"}
     validates :Direccion, format: { with: /https:\/\/goo.gl\/maps/, message: "tiene que ser un link de google maps" }
