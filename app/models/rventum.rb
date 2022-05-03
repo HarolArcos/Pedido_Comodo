0b148d0675493846da4e8f3b0ef8e65ce248f277
GonzaZeballos <101687891+GonzaZeballos@users.noreply.github.com>
2022-05-03 19:24:11 -0400

    Correccion validacion nombre en rventa

--- app/models/rventum.rb
+++ app/models/rventum.rb
@@ -1,19 +1,51 @@
 class Validar_Nombree < ActiveModel::Validator
     def validate (record)
        #validaciones de Nombre
-       if record.Nombre==nil || record.Nombre==""    
-           record.errors.add(:Nombre, "debe estar llenado")
-       else
-           if record.Nombre =~ /\A[a-zA-Z-ÿ\u00f1\u00d1\u00E0-\u00FC]{2,}(?:[\s-]*[a-zA-Z-ÿ\u00f1\u00d1\u00E0-\u00FC]{2,})?(?:[\s-]*[a-zA-Z-ÿ\u00f1\u00d1\u00E0-\u00FC]{2,})?(?:[\s-])?\z/
-               if record.Nombre.length()>21 
-                   record.errors.add(:Nombre, "tiene que tener maximo 21 caracteres")
-               end
-           else 
-               record.errors.add(:Nombre, "solo acepta letras y máximo 3 palabras, cada palabra debe tener mínimo 2 letras")
-           end
-       end
+       if record.Nombre==nil || record.Nombre=="" 
+        record.errors.add(:Nombre, "* Debe estar llenado")
+    else
+        
+            if record.Nombre =~ /\A[a-zA-Z-ÿ\u00f1\u00d1\u00E0-\u00FC]{2,}(?:[\s-]{1}[a-zA-Z-ÿ\u00f1\u00d1\u00E0-\u00FC]{2,})?(?:[\s-]{1}[a-zA-Z-ÿ\u00f1\u00d1\u00E0-\u00FC]{2,})?(?:[\s-])?\z/
+                if record.Nombre.length()>21 
+                    record.errors.add(:Nombre, "*Tiene que tener máximo 21 caracteres")
+                end
+            else 
+                if record.Nombre.start_with?(" ")
+                    record.errors.add(:Nombre, "*No debe iniciar con un espacio")
+                else
+                    if record.Nombre =~ /\s\s+/
+                        record.errors.add(:Nombre, "*Solo se acepta 1 espacio despues de un Nombre ")
+                    else
+
+                            if record.Nombre =~ /\A[a-zA-Z-ÿ\u00f1\u00d1\u00E0-\u00FC]{2,}(?:[\s-]{1}[a-zA-Z-ÿ\u00f1\u00d1\u00E0-\u00FC]{2,})?(?:[\s-]{1}[a-zA-Z-ÿ\u00f1\u00d1\u00E0-\u00FC]{2,})?(?:[\s-]{1}[a-zA-Z-ÿ\u00f1\u00d1\u00E0-\u00FC]{2,})?(?:[\s-]{1}[a-zA-Z-ÿ\u00f1\u00d1\u00E0-\u00FC]{2,})?(?:[\s-])?\z/
+                                #/\A[a-zA-Z-ÿ\u00f1\u00d1\u00E0-\u00FC]{2,}(?:[\s-]{1}[a-zA-Z-ÿ\u00f1\u00d1\u00E0-\u00FC]{2,})?(?:[\s-]{1}[a-zA-Z-ÿ\u00f1\u00d1\u00E0-\u00FC]{2,})+(?:[\s-])+\z/
+                                record.errors.add(:Nombre, "*Solo acepta máximo 3 palabras")
+                            else
+                                
+                                if record.Nombre =~ /[0-9]/
+                                    record.errors.add(:Nombre, "*Solo acepta letras ")
+
+                                else
+                                
+                                    if record.Nombre =~ /[a-zA-Z]/ 
+                                        record.errors.add(:Nombre, "*Cada nombre deve tener mínimo 2 letras")
+
+                                    else
+                                    
+                                        record.errors.add(:Nombre, "*Solo acepta letras ")
+                                    end
+                                end
+                                    
+                            end
+                     end
+                end
+            end
+        
+    end
    
        #validaciones de responsable
+
+       
        if record.Responsable==nil || record.Responsable==""
            record.errors.add(:"Responsable", " debe estar llenado")
        else
