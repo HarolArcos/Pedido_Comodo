a132c1735e8cb86c212f0add2d15964b1a67cceb
JorgeBeltr <201502274@est.umss.edu>
2022-05-14 15:44:59 -0400

    Bugs

--- db/migrate/20220426160810_create_rventa.rb
+++ db/migrate/20220426160810_create_rventa.rb
@@ -3,7 +3,7 @@ class CreateRventa < ActiveRecord::Migration[7.0]
     create_table :rventa do |t|
       t.string :Nombre
       t.string :Responsable
-      t.string :Telefono
+      t.integer :Telefono
       t.string :Mail
       t.string :Direccion
 
